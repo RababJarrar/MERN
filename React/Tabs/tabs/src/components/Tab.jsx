56bbd87c72b339bf3af1c1707159f5bfaf8cecb0
RababJarrar <rababjarrar1997@yahoo.com>
2022-12-11 20:22:20 +0200

    modify

--- React/Tabs/tabs/src/components/Tab.jsx
+++ React/Tabs/tabs/src/components/Tab.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Tab = () => {
-  return (
-    <div>
-      
+    const array=["Tab1 ","Tab2 ","Tab3 "];
+    const[ selectedtab, setSelectedtab]=useState("")
+    const display = x =>{
+        console.log(x)
+        setSelectedtab(x+"is shown here")
+    }  
+    return (
+    <div >
+      {array.map((item, id)=><span onClick={e=>display(item)} key={id}>{item}</span>)}
+      <hr></hr>
+      <p>{selectedtab}</p>
     </div>
   )
 }
