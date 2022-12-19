7699115bda14c8f348c559942ff9e38a4b89d1fb
RababJarrar <rababjarrar1997@yahoo.com>
2022-12-19 16:00:05 +0200

    add files

---
+++ Full Stack MERN/Product Manager (Part I)/client/src/components/ProductForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+export default () => {
+    //keep track of what is being typed via useState hook
+    const [Title, setTitle] = useState(""); 
+    const [Price, setPrice] = useState("");
+    const [ Description, setDescription] = useState("");
+    //handler when the form is submitted
+    const onSubmitHandler = e => {
+        //prevent default behavior of the submit
+        e.preventDefault();
+        //make a post request to create a new person
+        axios.post('http://localhost:8000/api/product', {   // route for create
+            Title,
+            Price,
+            Description
+        })
+            .then(res=>{console.log(res);setTitle("");    })  //setFirstName("");
+            .catch(err=>console.log(err))
+    }
+    //onChange to update firstName and lastName
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>Title</label><br/>
+                <input type="text" onChange={(e)=>setTitle(e.target.value)} value={Title}/>
+            </p>
+            <p>
+                <label>Price</label><br/>
+                <input type="text" onChange={(e)=>setPrice(e.target.value)} value={Price}/>
+            </p>
+            <p>
+                <label>Description</label><br/>
+                <input type="text" onChange={(e)=>setDescription(e.target.value)} value={Description}/>
+            </p>
+            <input type="submit"/>
+        </form>
+    )
+}
